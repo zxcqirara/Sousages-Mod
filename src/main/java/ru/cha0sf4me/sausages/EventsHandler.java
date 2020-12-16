6c39f1d9991461436b5c4ba20abafcfda9e7ab33
ChA0S_f4me <36239195+ChA0S-f4me@users.noreply.github.com>
2020-12-16 22:12:36 +0300

    Some changes

--- src/main/java/ru/cha0sf4me/sausages/EventsHandler.java
+++ src/main/java/ru/cha0sf4me/sausages/EventsHandler.java
@@ -1,16 +1,10 @@
 package ru.cha0sf4me.sausages;
 
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.storage.loot.LootTable;
 import net.minecraft.world.storage.loot.LootTableList;
 import net.minecraftforge.event.LootTableLoadEvent;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-import javax.xml.soap.Text;
 
 public class EventsHandler {
     @SubscribeEvent
